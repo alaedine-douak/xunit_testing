0c97513c9104b362b795fd8b151b5193a98c2668
Alaedindouak <alaedin.hak@gmail.com>
2025-11-27 15:26:00 +0100

    part 2.1: naming & documentations

---
+++ src/MyProject.Core/EmailValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyProject.Core;
+
+public class EmailValidator
+{
+
+   private readonly Regex _regex;
+
+   private const string EMAIL_PATTERN = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+   public EmailValidator()
+   {
+      _regex = new Regex(
+         EMAIL_PATTERN, 
+         RegexOptions.Compiled | RegexOptions.IgnoreCase);       
+   }
+
+   public bool IsValid(string email)
+   {
+      if (string.IsNullOrWhiteSpace(email)) return false;
+
+      return _regex.IsMatch(email);
+   }
+}
---
+++ tests/MyProject.Core.Tests/EmailValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿namespace MyProject.Core.Tests;
+
+public class EmailValidatorTests
+{
+
+   // Naming & Documentation
+
+   // MethodName_Scenario_ExpectedResult
+   // Pattern breakdown:
+   // MethodName: `IsValid` - What method/functionality is being tested
+   // Scenario: `WhenIsAValidEmail` - What conditions or input state
+   // ExpectedResult: `ShouldReturnTrue` - What should happen
+
+
+   [Fact(DisplayName = "Should return true if email is null")]
+   public void IsValid_WhenIsAValidEmail_ShouldReturnTrue()
+   {
+      var validator = new EmailValidator();
+
+      var result = validator.IsValid("user@example.com");
+
+      Assert.True(result);
+   }
+
+   [Fact]
+   public void IsValid_WhenIsAnInvalidEmail_ShouldReturnFalse()
+   {
+      var validator = new EmailValidator();
+
+      var result = validator.IsValid("invalid");
+
+      Assert.False(result);
+   }
+
+   [Fact]
+   public void IsValid_WhenEmailIsEmpty_ShouldReturnFalse()
+   {
+      var validator = new EmailValidator();
+
+      var result = validator.IsValid("");
+
+      Assert.False(result);
+   }
+}
